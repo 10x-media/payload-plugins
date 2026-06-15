1ce3ad3b51d1f64ca10385252963c60e999ffd3d
Harley Salas <harley@harleysalas.com>
2026-06-15 21:06:48 +0200

    feat(form-builder): add the shadcn renderers map and FormBuilderForm wrapper

---
+++ packages/form-builder/registry/form-builder/renderers.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import type { FieldRenderer, RenderersConfig } from '@10x-media/form-builder/react'
+import { checkboxField } from './fields/checkbox-field'
+import { emailField } from './fields/email-field'
+import { numberField } from './fields/number-field'
+import { selectField } from './fields/select-field'
+import { textField } from './fields/text-field'
+import { textareaField } from './fields/textarea-field'
+
+/** shadcn-styled renderers keyed by field-type slug. Pass to `<Form renderers>` or use `<FormBuilderForm>`. */
+export const shadcnRenderers: RenderersConfig = {
+	text: textField as FieldRenderer,
+	textarea: textareaField as FieldRenderer,
+	email: emailField as FieldRenderer,
+	number: numberField as FieldRenderer,
+	select: selectField as FieldRenderer,
+	checkbox: checkboxField as FieldRenderer,
+}
