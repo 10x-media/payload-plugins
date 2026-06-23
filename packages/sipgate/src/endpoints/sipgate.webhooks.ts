d3f5be3e823c32ab272459d27cf6502a0447204b
Xenoant <dpines2016@gmail.com>
2026-06-23 13:33:39 +0700

    feat(sipgate): enhance plugin options and refactor webhooks integration

--- packages/sipgate/src/endpoints/sipgate.webhooks.ts
+++ packages/sipgate/src/endpoints/sipgate.webhooks.ts
@@ -1,16 +1,15 @@
-import type { Endpoint } from 'payload'
+import { type CollectionSlug, deepMerge, type Endpoint } from 'payload'
 import { sipgateWebhookHandler } from '../utils/sipgateWebhookHandler'
 
-type SipgateWebhooks = {
-	endpoints: Endpoint[]
-}
-
-export const sipgateWebhooks: SipgateWebhooks = {
-	endpoints: [
-		{
-			path: '/sipgate/webhooks',
-			method: 'post',
-			handler: sipgateWebhookHandler,
-		},
-	],
+export const createSipgateWebhooks = (
+	contactCollections: CollectionSlug[],
+	phoneNumberFields: string[],
+	overrides?: Partial<Endpoint>
+): Endpoint => {
+	const defaultEndpoint: Endpoint = {
+		path: '/sipgate/webhooks',
+		method: 'post',
+		handler: sipgateWebhookHandler(contactCollections, phoneNumberFields),
+	}
+	return deepMerge<Endpoint>(defaultEndpoint, overrides ?? {})
 }
