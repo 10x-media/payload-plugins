11aa680e8c9690c0cb4e28f622e86237f3571363
Xenoant <dpines2016@gmail.com>
2026-07-07 11:18:52 +0700

    fix: related contact resolution

--- packages/sipgate/dev/app/(payload)/admin/importMap.js
+++ packages/sipgate/dev/app/(payload)/admin/importMap.js
@@ -1,6 +1,7 @@
 import { default as default_c37ba5ae51ab18b6fcc377bc66ff4d40 } from '@10x-media/sipgate/ui/SipgateOAuthButton'
 import { default as default_173e52a774727b0699f19bdd057db9b5 } from '@10x-media/sipgate/ui/ClickToDialField'
 import { default as default_ccea2224e3bb7b74b88cee7a622760ee } from '@10x-media/sipgate/ui/ContactMatchUiField'
+import { SipgateCallLogsSyncButton as SipgateCallLogsSyncButton_f6d11cbb76d23683198ab9de5dd0aff5 } from '@10x-media/sipgate/ui/SipgateSyncButton'
 import { SipgateUsersSyncButton as SipgateUsersSyncButton_f6d11cbb76d23683198ab9de5dd0aff5 } from '@10x-media/sipgate/ui/SipgateSyncButton'
 import { SipgateDevicesSyncButton as SipgateDevicesSyncButton_f6d11cbb76d23683198ab9de5dd0aff5 } from '@10x-media/sipgate/ui/SipgateSyncButton'
 import { SipgateChannelsSyncButton as SipgateChannelsSyncButton_f6d11cbb76d23683198ab9de5dd0aff5 } from '@10x-media/sipgate/ui/SipgateSyncButton'
@@ -13,6 +14,7 @@ export const importMap = {
   "@10x-media/sipgate/ui/SipgateOAuthButton#default": default_c37ba5ae51ab18b6fcc377bc66ff4d40,
   "@10x-media/sipgate/ui/ClickToDialField#default": default_173e52a774727b0699f19bdd057db9b5,
   "@10x-media/sipgate/ui/ContactMatchUiField#default": default_ccea2224e3bb7b74b88cee7a622760ee,
+  "@10x-media/sipgate/ui/SipgateSyncButton#SipgateCallLogsSyncButton": SipgateCallLogsSyncButton_f6d11cbb76d23683198ab9de5dd0aff5,
   "@10x-media/sipgate/ui/SipgateSyncButton#SipgateUsersSyncButton": SipgateUsersSyncButton_f6d11cbb76d23683198ab9de5dd0aff5,
   "@10x-media/sipgate/ui/SipgateSyncButton#SipgateDevicesSyncButton": SipgateDevicesSyncButton_f6d11cbb76d23683198ab9de5dd0aff5,
   "@10x-media/sipgate/ui/SipgateSyncButton#SipgateChannelsSyncButton": SipgateChannelsSyncButton_f6d11cbb76d23683198ab9de5dd0aff5,
